192b42ac2ab5111d2f54ae7e2f63f3321b488b17
Josh Willox <joshwillox@gmail.com>
2025-02-12 11:56:11 +1100

    feat: add force shutdown entity

--- components/buttons/buttons.go
+++ components/buttons/buttons.go
@@ -5,12 +5,13 @@ import (
 )
 
 type Config struct {
-	Custom   *CustomConfig  `yaml:"custom,omitempty"`
-	Lock     *entity.Config `yaml:"lock,omitempty"`
-	Sleep    *entity.Config `yaml:"sleep,omitempty"`
-	Shutdown *entity.Config `yaml:"shutdown,omitempty"`
-	Reload   *entity.Config `yaml:"reload,omitempty"`
-	Exit     *entity.Config `yaml:"exit,omitempty"`
+	Custom        *CustomConfig  `yaml:"custom,omitempty"`
+	Lock          *entity.Config `yaml:"lock,omitempty"`
+	Sleep         *entity.Config `yaml:"sleep,omitempty"`
+	Shutdown      *entity.Config `yaml:"shutdown,omitempty"`
+	ForceShutdown *entity.Config `yaml:"force_shutdown,omitempty"`
+	Reload        *entity.Config `yaml:"reload,omitempty"`
+	Exit          *entity.Config `yaml:"exit,omitempty"`
 }
 
 func (c *Config) LoadEntities() []*entity.Entity {
@@ -24,6 +25,9 @@ func (c *Config) LoadEntities() []*entity.Entity {
 	}
 	if c.Shutdown != nil {
 		entities = append(entities, NewShutdown(*c.Shutdown))
+	}
+	if c.ForceShutdown != nil {
+		entities = append(entities, NewForceShutdown(*c.ForceShutdown))
 	}
 	if c.Sleep != nil {
 		entities = append(entities, NewSleep(*c.Sleep))
